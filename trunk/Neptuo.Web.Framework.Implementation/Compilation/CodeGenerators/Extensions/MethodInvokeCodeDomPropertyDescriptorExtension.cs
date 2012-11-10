9e8f76a858185db70712d597a02595990af34b3f
SubversionUser <SubversionUser@43323ba4-dcaa-5c43-aabb-957627489be8>
2012-11-10 20:38:37 +0000

    Method invoke done

---
+++ trunk/Neptuo.Web.Framework.Implementation/Compilation/CodeGenerators/Extensions/MethodInvokeCodeDomPropertyDescriptorExtension.cs
@@ -0,0 +1,26 @@
+﻿using Neptuo.Web.Framework.Compilation.CodeObjects;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Neptuo.Web.Framework.Compilation.CodeGenerators.Extensions
+{
+    public class MethodInvokeCodeDomPropertyDescriptorExtension : BaseCodeDomPropertyDescriptorExtension<MethodInvokePropertyDescriptor>
+    {
+        protected override void GenerateProperty(CodeDomPropertyDescriptorExtensionContext context, MethodInvokePropertyDescriptor propertyDescriptor)
+        {
+            context.BindMethod.Statements.Add(
+                new CodeMethodInvokeExpression(
+                    new CodeFieldReferenceExpression(
+                        new CodeThisReferenceExpression(),
+                        context.FieldName
+                    ),
+                    propertyDescriptor.Method.Name,
+                    propertyDescriptor.Parameters.Select(p => new CodePrimitiveExpression(((PlainValueCodeObject)p).Value)).ToArray()
+                )
+            );
+        }
+    }
+}
