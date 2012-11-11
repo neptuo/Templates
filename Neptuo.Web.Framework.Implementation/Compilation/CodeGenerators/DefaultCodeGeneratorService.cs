ad0b3f2b9ad2ad07d3e729af35ef16e70f08ae0a
SubversionUser <Neptuo@users.noreply.github.com>
2012-11-11 14:07:56 +0000

    ICodeGeneratorService

---
+++ Neptuo.Web.Framework.Implementation/Compilation/CodeGenerators/DefaultCodeGeneratorService.cs
@@ -0,0 +1,26 @@
+﻿using Neptuo.Web.Framework.Compilation.CodeObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Neptuo.Web.Framework.Compilation.CodeGenerators
+{
+    public class DefaultCodeGeneratorService : ICodeGeneratorService
+    {
+        private Dictionary<string, ICodeGenerator> generators = new Dictionary<string, ICodeGenerator>();
+
+        public void AddGenerator(string name, ICodeGenerator generator)
+        {
+            generators[name] = generator;
+        }
+
+        public bool GeneratedCode(string name, IPropertyDescriptor propertyDescriptor, ICodeGeneratorContext context)
+        {
+            if (generators.ContainsKey(name))
+                return generators[name].ProcessTree(propertyDescriptor, context);
+
+            return false;
+        }
+    }
+}
---
+++ Neptuo.Web.Framework/Compilation/CodeGenerators/ICodeGeneratorService.cs
@@ -0,0 +1,29 @@
+﻿using Neptuo.Web.Framework.Compilation.CodeObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Neptuo.Web.Framework.Compilation.CodeGenerators
+{
+    /// <summary>
+    /// Service for generating code.
+    /// </summary>
+    public interface ICodeGeneratorService
+    {
+        /// <summary>
+        /// Registers <paramref name="generator"/> with <paramref name="name"/>.
+        /// </summary>
+        /// <param name="name">Name of generator.</param>
+        /// <param name="generator">Code generator.</param>
+        void AddGenerator(string name, ICodeGenerator generator);
+
+        /// <summary>
+        /// Generates code using code generator registered with <paramref name="name"/>.
+        /// </summary>
+        /// <param name="name">Name of generator.</param>
+        /// <param name="propertyDescriptor">Root property in AST.</param>
+        /// <param name="context">Context.</param>
+        bool GeneratedCode(string name, IPropertyDescriptor propertyDescriptor, ICodeGeneratorContext context);
+    }
+}
