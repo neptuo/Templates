35e0565fc42138a60ed431ebe68b5388a08a16db
SubversionUser <Neptuo@users.noreply.github.com>
2012-11-12 16:54:06 +0000

    Live compilation v1

---
+++ DemoWebUI/Models/LiveModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoWebUI.Models
+{
+    public class LiveModel
+    {
+        public string SessionID { get; set; }
+        [AllowHtml]
+        public string ViewContent { get; set; }
+    }
+}
