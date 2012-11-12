54767535849b36b34615f0132b04195826991f95
SubversionUser <SubversionUser@43323ba4-dcaa-5c43-aabb-957627489be8>
2012-11-12 17:40:47 +0000

    Live compilation v2

--- trunk/DemoWebUI/App_Start/BundleConfig.cs
+++ trunk/DemoWebUI/App_Start/BundleConfig.cs
@@ -11,6 +11,11 @@ namespace DemoWebUI
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/signalr")
+                .Include("~/signalr/hubs")
+                .Include("~/Scripts/jquery.signalR-{version}.js")
+            );
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
 
--- trunk/DemoWebUI/App_Start/RouteConfig.cs
+++ trunk/DemoWebUI/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DemoWebUI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Live", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
