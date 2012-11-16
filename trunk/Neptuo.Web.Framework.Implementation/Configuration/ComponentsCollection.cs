d48a22b59e3c69f564c395d07f5abc7d0ed0d3dd
SubversionUser <SubversionUser@43323ba4-dcaa-5c43-aabb-957627489be8>
2012-11-16 23:09:44 +0000

    Config file support

---
+++ trunk/Neptuo.Web.Framework.Implementation/Configuration/ComponentsCollection.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace Neptuo.Web.Framework.Configuration
+{
+    public class ComponentsCollection : ConfigurationElementCollection
+    {
+        public override ConfigurationElementCollectionType CollectionType
+        {
+            get { return ConfigurationElementCollectionType.AddRemoveClearMap; }
+        }
+
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new ComponentElement();
+        }
+
+        protected override Object GetElementKey(ConfigurationElement element)
+        {
+            return ((ComponentElement)element).Namespace;
+        }
+
+        public ComponentElement this[int index]
+        {
+            get { return (ComponentElement)BaseGet(index); }
+            set
+            {
+                if (BaseGet(index) != null)
+                    BaseRemoveAt(index);
+
+                BaseAdd(index, value);
+            }
+        }
+
+        new public ComponentElement this[string Name]
+        {
+            get { return (ComponentElement)BaseGet(Name); }
+        }
+
+        public int IndexOf(ComponentElement element)
+        {
+            return BaseIndexOf(element);
+        }
+
+        public void Add(ComponentElement element)
+        {
+            BaseAdd(element);
+        }
+
+        protected override void BaseAdd(ConfigurationElement element)
+        {
+            BaseAdd(element, false);
+        }
+
+        public void Remove(ComponentElement element)
+        {
+            if (BaseIndexOf(element) >= 0)
+                BaseRemove(element.Namespace);
+        }
+
+        public void RemoveAt(int index)
+        {
+            BaseRemoveAt(index);
+        }
+
+        public void Remove(string name)
+        {
+            BaseRemove(name);
+        }
+
+        public void Clear()
+        {
+            BaseClear();
+        }
+    }
+}
