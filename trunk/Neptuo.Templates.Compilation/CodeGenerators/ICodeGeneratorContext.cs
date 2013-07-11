2ca047e0b3ddcdbec1ebfc38b1237cef88c4c666
Mara <Mara@43323ba4-dcaa-5c43-aabb-957627489be8>
2013-07-11 20:32:52 +0000

    Testing new project structure

---
+++ trunk/Neptuo.Templates.Compilation/CodeGenerators/ICodeGeneratorContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Neptuo.Web.Framework.Compilation.CodeGenerators
+{
+    public interface ICodeGeneratorContext
+    {
+        ICodeGeneratorService CodeGeneratorService { get; }
+        IDependencyProvider DependencyProvider { get; }
+        ICollection<IErrorInfo> Errors { get; }
+        TextWriter Output { get; }
+    }
+}
---
+++ trunk/Neptuo.Templates.Compilation/IFileProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Neptuo.Web.Framework.Compilation
+{
+    public interface IFileProvider
+    {
+        bool Exists(string relativePath);
+        string GetFileContent(string relativePath);
+    }
+}
