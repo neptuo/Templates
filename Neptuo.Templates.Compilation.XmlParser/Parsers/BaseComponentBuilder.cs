87f2e8c2e45a61634d08de653276bd3b167633ed
Mara <Neptuo@users.noreply.github.com>
2013-07-28 07:21:12 +0000

    BaseComponentBuilder + BaseControlBuilder

---
+++ Neptuo.Templates.Compilation.XmlParser/Parsers/BaseComponentBuilder.cs
@@ -0,0 +1,31 @@
+﻿using Neptuo.Templates.Compilation.CodeObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Neptuo.Templates.Compilation.Parsers
+{
+    public abstract class BaseComponentBuilder : IComponentBuilder
+    {
+        public void Parse(IBuilderContext context, XmlElement element)
+        {
+            IComponentCodeObject codeObject = CreateCodeObject(context, element);
+            BindProperties(context, codeObject, element);
+            AppendToParent(context, codeObject);
+        }
+
+        protected virtual void AppendToParent(IBuilderContext context, IComponentCodeObject codeObject)
+        {
+            context.Parent.SetValue(codeObject);
+        }
+
+        protected abstract IComponentCodeObject CreateCodeObject(IBuilderContext context, XmlElement element);
+
+        protected abstract void BindProperties(IBuilderContext context, IComponentCodeObject codeObject, XmlElement element);
+
+        protected abstract void ProcessUnboundAttributes(IBuilderContext context, IComponentCodeObject codeObject, List<XmlAttribute> unboundAttributes);
+    }
+}
