2eb7bef12b68934025e93cdc1222bd6410018376
Mara <Mara@43323ba4-dcaa-5c43-aabb-957627489be8>
2013-07-30 18:42:33 +0000

    Separation of builders

--- trunk/Neptuo.Templates.Compilation.XmlParser/Parsers/BaseComponentBuilder.cs
+++ trunk/Neptuo.Templates.Compilation.XmlParser/Parsers/BaseComponentBuilder.cs
@@ -1,5 +1,7 @@
 ﻿using Neptuo.Templates.Compilation.CodeObjects;
+using Neptuo.Xml;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,8 +26,178 @@ namespace Neptuo.Templates.Compilation.Parsers
 
         protected abstract IComponentCodeObject CreateCodeObject(IBuilderContext context, XmlElement element);
 
-        protected abstract void BindProperties(IBuilderContext context, IComponentCodeObject codeObject, XmlElement element);
+        protected abstract IComponentDefinition GetComponentDefinition(IBuilderContext context, IComponentCodeObject codeObject, XmlElement element);
 
-        protected abstract void ProcessUnboundAttributes(IBuilderContext context, IComponentCodeObject codeObject, List<XmlAttribute> unboundAttributes);
+        protected abstract IPropertyDescriptor CreateSetPropertyDescriptor(IPropertyInfo propertyInfo);
+        protected abstract IPropertyDescriptor CreateListAddPropertyDescriptor(IPropertyInfo propertyInfo);
+
+        protected override void BindProperties(IBuilderContext context, IComponentCodeObject codeObject, XmlElement element)
+        {
+            IComponentDefinition componentDefinition = GetComponentDefinition(context, codeObject, element);
+
+            HashSet<string> boundProperies = new HashSet<string>();
+            IPropertyInfo defaultProperty = componentDefinition.GetDefaultProperty();
+            List<XmlAttribute> boundAttributes = new List<XmlAttribute>();
+
+            foreach (IPropertyInfo propertyInfo in componentDefinition.GetProperties())
+            {
+                bool bound = false;
+                string propertyName = propertyInfo.Name.ToLowerInvariant();
+                foreach (XmlAttribute attribute in element.Attributes)
+                {
+                    if (propertyName == attribute.Name.ToLowerInvariant())
+                    {
+                        IPropertyDescriptor propertyDescriptor = CreateSetPropertyDescriptor(propertyInfo);
+                        bool result = context.ParserContext.ParserService.ProcessValue(
+                            attribute.Value,
+                            new DefaultParserServiceContext(context.ParserContext.DependencyProvider, propertyDescriptor, context.ParserContext.Errors)
+                        );
+
+                        if (!result)
+                            result = context.Parser.BindPropertyDefaultValue(propertyDescriptor);
+
+                        if (result)
+                        {
+                            codeObject.Properties.Add(propertyDescriptor);
+                            boundProperies.Add(propertyName);
+                            boundAttributes.Add(attribute);
+                            bound = true;
+                        }
+                    }
+                }
+
+                XmlNode child;
+                if (!bound && XmlContentParser.Utils.FindChildNode(element, propertyName, out child))
+                {
+                    ResolvePropertyValue(context, codeObject, propertyInfo, child.ChildNodes.ToEnumerable());
+                    boundProperies.Add(propertyName);
+                    bound = true;
+                }
+
+                //if (!bound && item.Value != defaultProperty)
+                //{
+                //    IPropertyDescriptor propertyDescriptor = new SetPropertyDescriptor(item.Value);
+                //    if (context.Parser.BindPropertyDefaultValue(propertyDescriptor))
+                //    {
+                //        codeObject.Properties.Add(propertyDescriptor);
+                //        boundProperies.Add(propertyName);
+                //        bound = true;
+                //    }
+                //}
+            }
+
+            List<XmlAttribute> unboundAttributes = new List<XmlAttribute>();
+            foreach (XmlAttribute attribute in element.Attributes)
+            {
+                if (!boundAttributes.Contains(attribute))
+                    unboundAttributes.Add(attribute);
+            }
+            ProcessUnboundAttributes(context, codeObject, unboundAttributes);
+
+            if (defaultProperty != null && !boundProperies.Contains(defaultProperty.Name.ToLowerInvariant()))
+            {
+                IEnumerable<XmlNode> defaultChildNodes = XmlContentParser.Utils.FindNotUsedChildNodes(element, boundProperies);
+                if (defaultChildNodes.Any())
+                {
+                    ResolvePropertyValue(context, codeObject, defaultProperty, defaultChildNodes);
+                }
+                else
+                {
+                    IPropertyDescriptor propertyDescriptor = CreateSetPropertyDescriptor(defaultProperty);
+                    if (context.Parser.BindPropertyDefaultValue(propertyDescriptor))
+                        codeObject.Properties.Add(propertyDescriptor);
+                }
+            }
+        }
+
+        protected abstract void ProcessUnboundAttributes(IBuilderContext context, IComponentCodeObject codeObject, List<XmlAttribute> unboundAttributes)
+        {
+            List<XmlAttribute> usedAttributes = new List<XmlAttribute>();
+            XmlContentParser.ObserverList observers = new XmlContentParser.ObserverList();
+            foreach (XmlAttribute attribute in unboundAttributes)
+            {
+                bool boundAttribute = false;
+
+                if (attribute.Prefix.ToLowerInvariant() == "xmlns")
+                    boundAttribute = true;
+
+                if (!boundAttribute)
+                {
+                    IObserverRegistration observer = context.BuilderRegistry.GetObserverBuilder(attribute.Prefix, attribute.LocalName);
+                    if (observer != null)
+                    {
+                        if (observer.Scope == ObserverBuilderScope.PerElement && observers.ContainsKey(observer))
+                            observers[observer].Attributes.Add(attribute);
+                        else
+                            observers.Add(new XmlContentParser.ParsedObserver(observer, attribute));
+
+                        boundAttribute = true;
+                    }
+                }
+
+                if (!boundAttribute)
+                    ProcessUnboundAttribute(context, codeObject, attribute);
+            }
+
+            context.Parser.AttachObservers(context, codeObject, observers);
+        }
+
+        protected abstract void ProcessUnboundAttribute(IBuilderContext context, IComponentCodeObject codeObject, XmlAttribute unboundAttribute);
+
+        protected virtual void ResolvePropertyValue(IBuilderContext context, IPropertiesCodeObject codeObject, IPropertyInfo propertyInfo, IEnumerable<XmlNode> content)
+        {
+            if (typeof(string) == propertyInfo.Type)
+            {
+                //Get string and add as plain value
+                StringBuilder contentValue = new StringBuilder();
+                foreach (XmlNode node in content)
+                    contentValue.Append(node.OuterXml);
+
+                IPropertyDescriptor propertyDescriptor = CreateSetPropertyDescriptor(propertyInfo);
+                propertyDescriptor.SetValue(new PlainValueCodeObject(contentValue.ToString()));
+                codeObject.Properties.Add(propertyDescriptor);
+            }
+            else if (typeof(ICollection).IsAssignableFrom(propertyInfo.Type)
+                || typeof(IEnumerable).IsAssignableFrom(propertyInfo.Type)
+                || (propertyInfo.Type.IsGenericType && typeof(ICollection<>).IsAssignableFrom(propertyInfo.Type.GetGenericTypeDefinition()))
+            )
+            {
+                //Collection item
+                IPropertyDescriptor propertyDescriptor = CreateListAddPropertyDescriptor(propertyInfo);
+                codeObject.Properties.Add(propertyDescriptor);
+                context.Parser.ProcessContent(context, propertyDescriptor, content);
+            }
+            else
+            {
+                // Count elements
+                IEnumerable<XmlElement> elements = content.OfType<XmlElement>();
+                if (elements.Any())
+                {
+                    // One XmlElement is ok
+                    if (elements.Count() == 1)
+                    {
+                        IPropertyDescriptor propertyDescriptor = CreateSetPropertyDescriptor(propertyInfo);
+                        codeObject.Properties.Add(propertyDescriptor);
+                        context.Parser.ProcessContent(context, propertyDescriptor, content);
+                    }
+                    else
+                    {
+                        //More elements can't be bound!
+                        throw new ArgumentException("Unbindable property!");
+                    }
+                }
+                else
+                {
+                    //Get string and add as plain value
+                    StringBuilder contentValue = new StringBuilder();
+                    foreach (XmlNode node in content)
+                        contentValue.Append(node.OuterXml);
+
+                    IPropertyDescriptor propertyDescriptor = CreateSetPropertyDescriptor(propertyInfo);
+                    propertyDescriptor.SetValue(new PlainValueCodeObject(contentValue.ToString()));
+                    codeObject.Properties.Add(propertyDescriptor);
+                }
+            }
+        }
     }
 }
--- trunk/Neptuo.Templates.Components.Compilation/Parsers/BaseControlBuilder.cs
+++ trunk/Neptuo.Templates.Components.Compilation/Parsers/BaseControlBuilder.cs
@@ -21,180 +21,24 @@ namespace Neptuo.Templates.Compilation.Parsers
             return new ControlCodeObject(GetControlType(element));
         }
 
-        protected override void BindProperties(IBuilderContext context, IComponentCodeObject codeObject, XmlElement element)
+        protected override IComponentDefinition GetComponentDefinition(IBuilderContext context, IComponentCodeObject codeObject, XmlElement element)
         {
-            IComponentDefinition componentDefinition = new TypeComponentDefinition(GetControlType(element));
-
-            HashSet<string> boundProperies = new HashSet<string>();
-            IPropertyInfo defaultProperty = componentDefinition.GetDefaultProperty();
-            List<XmlAttribute> boundAttributes = new List<XmlAttribute>();
-
-            foreach (IPropertyInfo propertyInfo in componentDefinition.GetProperties())
-            {
-                bool bound = false;
-                string propertyName = propertyInfo.Name.ToLowerInvariant();
-                foreach (XmlAttribute attribute in element.Attributes)
-                {
-                    if (propertyName == attribute.Name.ToLowerInvariant())
-                    {
-                        IPropertyDescriptor propertyDescriptor = new SetPropertyDescriptor(propertyInfo.Name);
-                        bool result = context.ParserContext.ParserService.ProcessValue(
-                            attribute.Value,
-                            new DefaultParserServiceContext(context.ParserContext.DependencyProvider, propertyDescriptor, context.ParserContext.Errors)
-                        );
-
-                        if (!result)
-                            result = context.Parser.BindPropertyDefaultValue(propertyDescriptor);
-
-                        if (result)
-                        {
-                            codeObject.Properties.Add(propertyDescriptor);
-                            boundProperies.Add(propertyName);
-                            boundAttributes.Add(attribute);
-                            bound = true;
-                        }
-                    }
-                }
-
-                XmlNode child;
-                if (!bound && XmlContentParser.Utils.FindChildNode(element, propertyName, out child))
-                {
-                    ResolvePropertyValue(context, codeObject, propertyInfo, child.ChildNodes.ToEnumerable());
-                    boundProperies.Add(propertyName);
-                    bound = true;
-                }
-
-                //if (!bound && item.Value != defaultProperty)
-                //{
-                //    IPropertyDescriptor propertyDescriptor = new SetPropertyDescriptor(item.Value);
-                //    if (context.Parser.BindPropertyDefaultValue(propertyDescriptor))
-                //    {
-                //        codeObject.Properties.Add(propertyDescriptor);
-                //        boundProperies.Add(propertyName);
-                //        bound = true;
-                //    }
-                //}
-            }
-
-            List<XmlAttribute> unboundAttributes = new List<XmlAttribute>();
-            foreach (XmlAttribute attribute in element.Attributes)
-            {
-                if (!boundAttributes.Contains(attribute))
-                    unboundAttributes.Add(attribute);
-            }
-            ProcessUnboundAttributes(context, codeObject, unboundAttributes);
-
-            if (defaultProperty != null && !boundProperies.Contains(defaultProperty.Name.ToLowerInvariant()))
-            {
-                IEnumerable<XmlNode> defaultChildNodes = XmlContentParser.Utils.FindNotUsedChildNodes(element, boundProperies);
-                if (defaultChildNodes.Any())
-                {
-                    ResolvePropertyValue(context, codeObject, defaultProperty, defaultChildNodes);
-                }
-                else
-                {
-                    IPropertyDescriptor propertyDescriptor = new SetPropertyDescriptor(defaultProperty.Name);
-                    if (context.Parser.BindPropertyDefaultValue(propertyDescriptor))
-                        codeObject.Properties.Add(propertyDescriptor);
-                }
-            }
+            return new TypeComponentDefinition(GetControlType(element));
         }
 
-        protected override void ProcessUnboundAttributes(IBuilderContext context, IComponentCodeObject codeObject, List<XmlAttribute> unboundAttributes)
+        protected override void ProcessUnboundAttribute(IBuilderContext context, IComponentCodeObject codeObject, XmlAttribute attribute)
         {
-            XmlContentParser.ObserverList observers = new XmlContentParser.ObserverList();
-            foreach (XmlAttribute attribute in unboundAttributes)
-            {
-                bool boundAttribute = false;
-
-                if (attribute.Prefix.ToLowerInvariant() == "xmlns")
-                    boundAttribute = true;
-
-                if (!boundAttribute)
-                {
-                    IObserverRegistration observer = context.BuilderRegistry.GetObserverBuilder(attribute.Prefix, attribute.LocalName);
-                    if (observer != null)
-                    {
-                        if (observer.Scope == ObserverBuilderScope.PerElement && observers.ContainsKey(observer))
-                            observers[observer].Attributes.Add(attribute);
-                        else
-                            observers.Add(new XmlContentParser.ParsedObserver(observer, attribute));
-
-                        boundAttribute = true;
-                    }
-                }
-
-                if (!boundAttribute && typeof(IAttributeCollection).IsAssignableFrom(codeObject.Type))
-                    boundAttribute = BindAttributeCollection(context, codeObject, codeObject, attribute.LocalName, attribute.Value);
-            }
-
-            context.Parser.AttachObservers(context, codeObject, observers);
+            BindAttributeCollection(context, codeObject, codeObject, attribute.LocalName, attribute.Value);
         }
 
-        protected virtual void ResolvePropertyValue(
-            IBuilderContext context, 
-            IPropertiesCodeObject codeObject, 
-            IPropertyInfo propertyInfo, 
-            IEnumerable<XmlNode> content)
+        protected override IPropertyDescriptor CreateListAddPropertyDescriptor(IPropertyInfo propertyInfo)
         {
-            if (typeof(string) == propertyInfo.Type)
-            {
-                //Get string and add as plain value
-                StringBuilder contentValue = new StringBuilder();
-                foreach (XmlNode node in content)
-                    contentValue.Append(node.OuterXml);
+            return new ListAddPropertyDescriptor(propertyInfo.Name);
+        }
 
-                codeObject.Properties.Add(
-                    new SetPropertyDescriptor(
-                        propertyInfo.Name,
-                        new PlainValueCodeObject(contentValue.ToString())
-                    )
-                );
-            }
-            else if (typeof(ICollection).IsAssignableFrom(propertyInfo.Type)
-                || typeof(IEnumerable).IsAssignableFrom(propertyInfo.Type)
-                || (propertyInfo.Type.IsGenericType && typeof(ICollection<>).IsAssignableFrom(propertyInfo.Type.GetGenericTypeDefinition()))
-            )
-            {
-                //Collection item
-                IPropertyDescriptor propertyDescriptor = new ListAddPropertyDescriptor(propertyInfo.Name);
-                codeObject.Properties.Add(propertyDescriptor);
-                context.Parser.ProcessContent(context, propertyDescriptor, content);
-            }
-            else
-            {
-                // Count elements
-                IEnumerable<XmlElement> elements = content.OfType<XmlElement>();
-                if (elements.Any())
-                {
-                    // One XmlElement is ok
-                    if (elements.Count() == 1)
-                    {
-                        IPropertyDescriptor propertyDescriptor = new SetPropertyDescriptor(propertyInfo.Name);
-                        codeObject.Properties.Add(propertyDescriptor);
-                        context.Parser.ProcessContent(context, propertyDescriptor, content);
-                    }
-                    else
-                    {
-                        //More elements can't be bound!
-                        throw new ArgumentException("Unbindable property!");
-                    }
-                }
-                else
-                {
-                    //Get string and add as plain value
-                    StringBuilder contentValue = new StringBuilder();
-                    foreach (XmlNode node in content)
-                        contentValue.Append(node.OuterXml);
-
-                    codeObject.Properties.Add(
-                        new SetPropertyDescriptor(
-                            propertyInfo.Name,
-                            new PlainValueCodeObject(contentValue.ToString())
-                        )
-                    );
-                }
-            }
+        protected override IPropertyDescriptor CreateSetPropertyDescriptor(IPropertyInfo propertyInfo)
+        {
+            return new SetPropertyDescriptor(propertyInfo.Name);
         }
 
         public static bool BindAttributeCollection(IBuilderContext context, ITypeCodeObject typeCodeObject, IPropertiesCodeObject propertiesCodeObject, string name, string value)
