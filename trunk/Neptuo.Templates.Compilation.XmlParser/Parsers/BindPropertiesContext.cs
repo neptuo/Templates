b43de69013eeab552198e21fe040be8381462db2
Mara <Mara@43323ba4-dcaa-5c43-aabb-957627489be8>
2013-08-03 17:06:35 +0000

    XmlContentParser refactoring

---
+++ trunk/Neptuo.Templates.Compilation.XmlParser/Parsers/BindPropertiesContext.cs
@@ -0,0 +1,24 @@
+﻿using Neptuo.Templates.Compilation.CodeObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Neptuo.Templates.Compilation.Parsers
+{
+    public class BindPropertiesContext
+    {
+        public Dictionary<string, IPropertyInfo> Properties { get; private set; }
+        public HashSet<string> BoundProperies { get; private set; }
+        public List<XmlAttribute> UnboundAttributes { get; private set; }
+
+        public BindPropertiesContext(Dictionary<string, IPropertyInfo> properties)
+        {
+            Properties = properties;
+            BoundProperies = new HashSet<string>();
+            UnboundAttributes = new List<XmlAttribute>();
+        }
+    }
+}
