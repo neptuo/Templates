925db5b4e7b8691f74619ed2cec72d43b117cde8
Mara <Mara@43323ba4-dcaa-5c43-aabb-957627489be8>
2013-08-04 15:37:50 +0000

    Registration refactoring

---
+++ trunk/Neptuo.Templates.Components.Compilation/Parsers/DefaultControlBuilderFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Templates.Compilation.Parsers
+{
+    public class DefaultControlBuilderFactory : IComponentBuilderFactory
+    {
+        protected Type ControlType { get; private set; }
+
+        public DefaultControlBuilderFactory(Type controlType)
+        {
+            ControlType = controlType;
+        }
+
+        public IComponentBuilder CreateBuilder(string prefix, string tagName)
+        {
+            return new DefaultControlBuilder(ControlType);
+        }
+    }
+}
---
+++ trunk/Neptuo.Templates.Components.Compilation/Parsers/IComponentBuilderFactory.cs
@@ -0,0 +1,14 @@
+﻿using Neptuo.Templates.Compilation.Parsers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Templates.Compilation.Parsers
+{
+    public interface IComponentBuilderFactory
+    {
+        IComponentBuilder CreateBuilder(string prefix, string tagName);
+    }
+}
