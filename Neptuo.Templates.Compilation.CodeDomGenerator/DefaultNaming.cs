07baf8af026521feaa6941f37fa1788f58aac39c
Mara <Neptuo@users.noreply.github.com>
2013-08-04 15:19:54 +0000

    Naming refactoring, part 3

---
+++ Neptuo.Templates.Compilation.CodeDomGenerator/DefaultNaming.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Templates.Compilation
+{
+    public class DefaultNaming : INaming
+    {
+        public string ClassNamespace { get; set; }
+        public string ClassName { get; set; }
+        public string AssemblyName { get; set; }
+
+        public DefaultNaming(string classNamespace, string className, string assemblyName)
+        {
+            ClassNamespace = classNamespace;
+            ClassName = className;
+            AssemblyName = assemblyName;
+        }
+    }
+}
