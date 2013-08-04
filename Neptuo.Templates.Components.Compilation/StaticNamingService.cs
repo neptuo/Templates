7f9aeaab75e90db15c8fb26ffabb61d72a6239f8
Mara <Neptuo@users.noreply.github.com>
2013-08-04 14:57:30 +0000

    Naming refactoring, part 2

--- Neptuo.Templates.Components.Compilation/StaticNamingService.cs
+++ Neptuo.Templates.Components.Compilation/StaticNamingService.cs
@@ -10,6 +10,8 @@ namespace Neptuo.Templates.Compilation
 {
     public class StaticNamingService : INamingService
     {
+        public const string ViewNameFormat = "View_{0}";
+
         protected IFileProvider FileProvider { get; private set; }
 
         public StaticNamingService(IFileProvider fileProvider)
@@ -19,21 +21,36 @@ namespace Neptuo.Templates.Compilation
 
         public INaming FromFile(string fileName)
         {
-            return FromContent(FileProvider.GetFileContent(fileName));
+            return GetNaming(GetNameForFile(fileName));
         }
 
         public INaming FromContent(string viewContent)
         {
+            return GetNaming(GetNameForContent(viewContent));
+        }
+
+        protected virtual INaming GetNaming(string viewName)
+        {
             return new DefaultNaming(
-                CodeDomStructureGenerator.Names.CodeNamespace, 
-                CodeDomStructureGenerator.Names.ClassName, 
-                GetAssemblyNameForContent(viewContent)
+                CodeDomStructureGenerator.Names.CodeNamespace,
+                viewName,
+                GetAssemblyForName(viewName)
             );
         }
 
-        protected virtual string GetAssemblyNameForContent(string viewContent)
+        protected virtual string GetNameForFile(string fileName)
         {
-            return String.Format("View_{0}.dll", HashHelper.Sha1(viewContent));
+            return String.Format(ViewNameFormat, HashHelper.Sha1(FileProvider.GetFileContent(fileName)));
+        }
+
+        protected virtual string GetNameForContent(string viewContent)
+        {
+            return String.Format(ViewNameFormat, HashHelper.Sha1(viewContent));
+        }
+
+        protected virtual string GetAssemblyForName(string viewName)
+        {
+            return String.Format("{0}.dll", viewName);
         }
     }
 }
