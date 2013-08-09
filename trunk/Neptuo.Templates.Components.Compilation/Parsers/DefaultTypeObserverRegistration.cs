e38937e981a2d958314d0efe9c3ad9bc9b3f9013
Mara <Mara@43323ba4-dcaa-5c43-aabb-957627489be8>
2013-08-09 13:36:56 +0000

    Small refactor

---
+++ trunk/Neptuo.Templates.Components.Compilation/Parsers/DefaultTypeObserverRegistration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Templates.Compilation.Parsers
+{
+    public class DefaultTypeObserverRegistration : IObserverRegistration
+    {
+        protected Type ObserverType { get; private set; }
+        public ObserverBuilderScope Scope { get; set; }
+
+        public DefaultTypeObserverRegistration(Type observerType, ObserverBuilderScope scope)
+        {
+            ObserverType = observerType;
+            Scope = scope;
+        }
+
+        public IObserverBuilder CreateBuilder()
+        {
+            return new DefaultTypeObserverBuilder(ObserverType, GetLivecycleFromScope());
+        }
+
+        protected virtual ObserverLivecycle GetLivecycleFromScope()
+        {
+            switch (Scope)
+            {
+                case ObserverBuilderScope.PerAttribute:
+                    return ObserverLivecycle.PerAttribute;
+                case ObserverBuilderScope.PerElement:
+                    return ObserverLivecycle.PerControl;
+            }
+            return ObserverLivecycle.PerControl;
+        }
+    }
+}
