d778ae565faf77b8596d22bfcf57b3d3250c7e31
Mara <Neptuo@users.noreply.github.com>
2013-08-20 11:02:33 +0000

    Observer registration refactoring

--- Neptuo.Templates.Components.Compilation/Parsers/DefaultTypeObserverBuilderFactory.cs
+++ Neptuo.Templates.Components.Compilation/Parsers/DefaultTypeObserverBuilderFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Neptuo.Reflection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,18 +9,30 @@ namespace Neptuo.Templates.Compilation.Parsers
 {
     public class DefaultTypeObserverBuilderFactory : IObserverBuilderFactory
     {
-        protected Type ObserverType { get; private set; }
-        protected ObserverBuilderScope Scope { get; private set; }
+        protected IObserverRegistration ObserverRegistration { get; private set; }
 
         public DefaultTypeObserverBuilderFactory(Type observerType, ObserverBuilderScope scope)
         {
-            ObserverType = observerType;
-            Scope = scope;
+            ObserverRegistration = new DefaultTypeObserverRegistration(observerType, scope);
+        }
+
+        public DefaultTypeObserverBuilderFactory(Type observerType, ObserverLivecycle livecycle)
+        {
+            ObserverRegistration = new DefaultTypeObserverRegistration(observerType, livecycle);
+        }
+
+        public DefaultTypeObserverBuilderFactory(Type observerType)
+        {
+            ObserverAttribute attribute = ReflectionHelper.GetAttribute<ObserverAttribute>(observerType);
+            if (attribute == null)
+                throw new InvalidOperationException("Unnable to find livecycle or scope for observer. Use ObserverAttribute or speciafy it explicitly!");
+
+            ObserverRegistration = new DefaultTypeObserverRegistration(observerType, attribute.Livecycle);
         }
 
         public IObserverRegistration CreateBuilder(string prefix, string attributeName)
         {
-            return new DefaultTypeObserverRegistration(ObserverType, Scope);
+            return ObserverRegistration;
         }
     }
 }
--- TestConsoleNG/Observers/VisibleObserver.cs
+++ TestConsoleNG/Observers/VisibleObserver.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace TestConsoleNG.Observers
 {
+    [Observer(ObserverLivecycle.PerControl)]
     public class VisibleObserver : IObserver
     {
         public bool Visible { get; set; }
