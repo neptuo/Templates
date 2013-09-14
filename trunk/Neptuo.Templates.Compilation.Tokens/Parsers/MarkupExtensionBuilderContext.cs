3d659f0326fba7e2844ea360acdf35d87628528d
Mara <Mara@43323ba4-dcaa-5c43-aabb-957627489be8>
2013-09-14 07:56:41 +0000

    MarkupExtension -> Token

---
+++ trunk/Neptuo.Templates.Compilation.Tokens/Parsers/MarkupExtensionBuilderContext.cs
@@ -0,0 +1,27 @@
+﻿using Neptuo.Templates.Compilation.CodeObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Templates.Compilation.Parsers
+{
+    public class MarkupExtensionBuilderContext : IMarkupExtensionBuilderContext
+    {
+        public IValueParserContext ParserContext { get; private set; }
+        public IPropertyDescriptor Parent { get; private set; }
+        public MarkupExtensionValueParser Parser { get; private set; }
+        public MarkupExtensionValueParser.Helper Helper { get; private set; }
+        public IMarkupExtensionBuilderRegistry BuilderRegistry { get; private set; }
+
+        public MarkupExtensionBuilderContext(MarkupExtensionValueParser parser, MarkupExtensionValueParser.Helper helper)
+        {
+            Parser = parser;
+            Helper = helper;
+            ParserContext = helper.Context;
+            Parent = helper.Context.PropertyDescriptor;
+            BuilderRegistry = helper.BuilderRegistry;
+        }
+    }
+}
