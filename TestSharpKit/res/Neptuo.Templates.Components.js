db173c8ed0473688a8710900d5c745e7e6394c01
Mara <Neptuo@users.noreply.github.com>
2013-10-12 13:02:36 +0000

    VersionInfo

--- TestSharpKit/res/Neptuo.Templates.Components.js
+++ TestSharpKit/res/Neptuo.Templates.Components.js
@@ -1045,11 +1045,11 @@ var Neptuo$Templates$Components$VersionInfo =
     {
         cctor: function ()
         {
-            Neptuo.Templates.Components.VersionInfo.Version = "1.3.0";
+            Neptuo.Templates.Components.VersionInfo.Version = "2.0.0";
         },
         GetVersion: function ()
         {
-            return new System.Version.ctor$$String("1.3.0");
+            return new System.Version.ctor$$String("2.0.0");
         }
     },
     assemblyName: "Neptuo.Templates.Components",
