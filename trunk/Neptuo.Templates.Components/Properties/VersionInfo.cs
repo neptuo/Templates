dd86433f9cfd42ddf48c49fa9edd27020977e302
Mara <Mara@43323ba4-dcaa-5c43-aabb-957627489be8>
2013-11-17 11:13:29 +0000

    Fixed usage of MethodInvokePropertyDescriptor with ValueExtenion

--- trunk/Neptuo.Templates.Components/Properties/VersionInfo.cs
+++ trunk/Neptuo.Templates.Components/Properties/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace Neptuo.Templates.Components
 {
     public static class VersionInfo
     {
-        internal const string Version = "2.9.1";
+        internal const string Version = "2.9.3";
 
         public static Version GetVersion()
         {
