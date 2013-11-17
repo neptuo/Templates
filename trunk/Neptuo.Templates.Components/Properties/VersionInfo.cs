b6d94f81bc6ece5379a9d83602b8c98dc64af076
Mara <Mara@43323ba4-dcaa-5c43-aabb-957627489be8>
2013-11-17 10:13:34 +0000

    Fix using markup extension in method

--- trunk/Neptuo.Templates.Components/Properties/VersionInfo.cs
+++ trunk/Neptuo.Templates.Components/Properties/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace Neptuo.Templates.Components
 {
     public static class VersionInfo
     {
-        internal const string Version = "2.9.0";
+        internal const string Version = "2.9.1";
 
         public static Version GetVersion()
         {
