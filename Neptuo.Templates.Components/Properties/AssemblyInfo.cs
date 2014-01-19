b39216c5739cacaac58fd5054564f30a44359747
Mara <Neptuo@users.noreply.github.com>
2014-01-19 13:45:23 +0000

    SharpKit Export using Exugin

--- Neptuo.Templates.Components/Properties/AssemblyInfo.cs
+++ Neptuo.Templates.Components/Properties/AssemblyInfo.cs
@@ -39,10 +39,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyInformationalVersion(VersionInfo.Version)]
 [assembly: AssemblyFileVersion(VersionInfo.Version)]
 [assembly: InternalsVisibleTo("Neptuo.Templates.Components.Compilation")]
-
-[assembly: JsExport(FilenameFormat = "../TestSharpKit/res/{0}.js")]
-[assembly: JsType(JsMode.Clr, Filename = "~/Neptuo.Templates.Components")]
-[assembly: JsType(TargetType = typeof(ObserverAttribute), Export = false)]
-//[assembly: JsType(TargetType = typeof(Neptuo.IDependencyProvider), Filename = "~/../TestSharpKit/res/Neptuo.Templates.Components.js")]
-//[assembly: JsType(TargetType = typeof(Neptuo.IDependencyContainer), Filename = "~/../TestSharpKit/res/Neptuo.Templates.Components.js")]
-//[assembly: JsType(TargetType = typeof(Neptuo.DependencyContainerExtensions), Filename = "~/../TestSharpKit/res/Neptuo.Templates.Components.js")]
