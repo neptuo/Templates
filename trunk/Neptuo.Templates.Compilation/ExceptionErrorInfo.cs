51303a2af6c4f2e41425fe0f92d2e03974b1264d
Mara <Mara@43323ba4-dcaa-5c43-aabb-957627489be8>
2014-09-25 15:05:43 +0000

    Comments.

--- trunk/Neptuo.Templates.Compilation/ExceptionErrorInfo.cs
+++ trunk/Neptuo.Templates.Compilation/ExceptionErrorInfo.cs
@@ -6,19 +6,37 @@ using System.Threading.Tasks;
 
 namespace Neptuo.Templates.Compilation
 {
+    /// <summary>
+    /// Extesion of <see cref="ErrorInfo"/> with an exception which caused the error.
+    /// </summary>
     public class ExceptionErrorInfo : ErrorInfo
     {
-        public Exception Exception { get; set; }
+        /// <summary>
+        /// The exception which caused the error.
+        /// </summary>
+        public Exception Exception { get; private set; }
 
+        /// <summary>
+        /// Creates new instance with <paramref name="exception"/>.
+        /// </summary>
+        /// <param name="exception">The exception which caused the error.</param>
         public ExceptionErrorInfo(Exception exception)
             : base(exception.Message)
         {
+            Guard.NotNull(exception, "exception");
             Exception = exception; 
         }
 
+        /// <summary>
+        /// Creates new instance with <paramref name="exception"/>.
+        /// </summary>
+        /// <param name="line">Line number of error.</param>
+        /// <param name="column">Column index of error.</param>
+        /// <param name="exception">The exception which caused the error.</param>
         public ExceptionErrorInfo(int line, int column, Exception exception)
             : base(line, column, exception.Message)
         {
+            Guard.NotNull(exception, "exception");
             Exception = exception;
         }
     }
