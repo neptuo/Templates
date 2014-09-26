f150644e0c8f59ed577f91e17cdb9cd3048f6072
Mara <Mara@43323ba4-dcaa-5c43-aabb-957627489be8>
2014-09-26 09:57:11 +0000

    Better implementation of TypeBuilderRegistry "scopes".

---
+++ trunk/Neptuo.Templates.Components.Compilation/Parsers/TypeBuilderRegistryException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Templates.Compilation.Parsers
+{
+    /// <summary>
+    /// Exception thrown from <see cref="TypeBuilderRegistry"/>.
+    /// </summary>
+    [Serializable]
+    public class TypeBuilderRegistryException : Exception
+    {
+        public TypeBuilderRegistryException(string message) 
+            : base(message) 
+        { }
+
+        public TypeBuilderRegistryException(string message, Exception inner) 
+            : base(message, inner) 
+        { }
+
+        protected TypeBuilderRegistryException(SerializationInfo info, StreamingContext context)
+            : base(info, context) 
+        { }
+    }
+}
