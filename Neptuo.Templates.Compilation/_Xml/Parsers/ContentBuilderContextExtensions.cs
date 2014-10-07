9dff62d19f2ddbef8ef2820deaf40d964dd9e3a1
Mara <Neptuo@users.noreply.github.com>
2014-10-07 06:06:39 +0000

    Some extensions for IContentBuilderContext.

---
+++ Neptuo.Templates.Compilation/_Xml/Parsers/ContentBuilderContextExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Templates.Compilation.Parsers
+{
+    /// <summary>
+    /// Some common extensions for <see cref="IContentBuilderContext"/>.
+    /// </summary>
+    public static class ContentBuilderContextExtensions
+    {
+        /// <summary>
+        /// Adds error to parser context in context.
+        /// </summary>
+        /// <param name="context">Builder context.</param>
+        /// <param name="errorInfo">Error description.</param>
+        public static void AddError(this IContentBuilderContext context, IErrorInfo errorInfo)
+        {
+            Guard.NotNull(context, "context");
+            Guard.NotNull(errorInfo, "errorInfo");
+            context.ParserContext.Errors.Add(errorInfo);
+        }
+
+        /// <summary>
+        /// Adds error to parser context in context.
+        /// </summary>
+        /// <param name="context">Builder context.</param>
+        /// <param name="errorText">Description of error.</param>
+        public static void AddError(this IContentBuilderContext context, string errorText)
+        {
+            Guard.NotNull(context, "context");
+            context.AddError(new ErrorInfo(errorText));
+        }
+
+        /// <summary>
+        /// Adds error to parser context in context.
+        /// </summary>
+        /// <param name="context">Builder context.</param>
+        /// <param name="line">Source file line number.</param>
+        /// <param name="column">Source file line column number.</param>
+        /// <param name="errorText">Description of error.</param>
+        public static void AddError(this IContentBuilderContext context, int line, int column, string errorText)
+        {
+            Guard.NotNull(context, "context");
+            context.AddError(new ErrorInfo(line, column, errorText));
+        }
+    }
+}
