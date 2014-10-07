4f681f5bf5bdd67bd086d27260c9747784d7851a
Mara <Mara@43323ba4-dcaa-5c43-aabb-957627489be8>
2014-10-07 06:06:39 +0000

    Some extensions for IContentBuilderContext.

--- trunk/Neptuo.Templates.Compilation/Properties/VersionInfo.cs
+++ trunk/Neptuo.Templates.Compilation/Properties/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace Neptuo.Templates.Compilation
 {
     public static class VersionInfo
     {
-        internal const string Version = "0.9.3";
+        internal const string Version = "0.9.4";
 
         public static Version GetVersion()
         {
