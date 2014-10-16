3a6749e7090e14aeb780ed794531b539ec9af3bc
Mara <Mara@43323ba4-dcaa-5c43-aabb-957627489be8>
2014-10-16 08:28:20 +0000

    Add NotSupportedContentBuilder and NotSupportedTokenBuilder for handling unknown xml tags and tokens.

---
+++ trunk/Neptuo.Templates.Compilation/_Xml/Parsers/NotSupportedContentBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Neptuo.Templates.Compilation.CodeObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Templates.Compilation.Parsers
+{
+    /// <summary>
+    /// Implementation of <see cref="IContentBuilder"/> that for all xml elements returns 'This element is not supported'.
+    /// </summary>
+    public class NotSupportedContentBuilder : IContentBuilder
+    {
+        public ICodeObject Parse(IContentBuilderContext context, IXmlElement element)
+        {
+            context.AddError(String.Format("Xml element '{0}' is not supported.", element.Name));
+            return null;
+        }
+    }
+}
