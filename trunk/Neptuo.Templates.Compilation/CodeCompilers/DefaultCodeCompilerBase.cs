73210a7c4084c45bba7e34b412b1637c8a60b736
Mara <Mara@43323ba4-dcaa-5c43-aabb-957627489be8>
2014-10-26 22:06:39 +0000

    Added base default implementation of default compiler.

---
+++ trunk/Neptuo.Templates.Compilation/CodeCompilers/DefaultCodeCompilerBase.cs
@@ -0,0 +1,66 @@
+﻿using Neptuo.Compilers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Templates.Compilation.CodeCompilers
+{
+    /// <summary>
+    /// Base for default implementation for compiling C# source code to the assemblies.
+    /// </summary>
+    public abstract class DefaultCodeCompilerBase : ICodeCompiler, ICompilerConfiguration
+    {
+        private readonly CompilerService compilerService;
+        private readonly string tempDirectory;
+
+        /// <summary>
+        /// Whether debug mode is on/off (.cs saving, pdb generation, etc.)
+        /// </summary>
+        public bool IsDebugMode
+        {
+            get { return compilerService.Factory.IsDebugMode; }
+            set { compilerService.Factory.IsDebugMode = value; }
+        }
+
+        /// <summary>
+        /// Compiler references.
+        /// </summary>
+        public CompilerReferenceCollection References
+        {
+            get { return compilerService.Factory.References; }
+        }
+
+        /// <summary>
+        /// Creates new instance which stores temp files to the <paramref name="tempDirectory"/>.
+        /// </summary>
+        /// <param name="tempDirectory">Directory for storing temp files (.cs, .dll, .pdb, etc).</param>
+        public DefaultCodeCompilerBase(string tempDirectory)
+        {
+            Guard.NotNullOrEmpty(tempDirectory, "tempDirectory");
+
+            if (Directory.Exists(tempDirectory))
+                throw Guard.Exception.ArgumentDirectoryNotExist(tempDirectory, "tempDirectory");
+
+            this.compilerService = CompilerService.FromCurrentAppDomain();
+            this.tempDirectory = tempDirectory;
+        }
+
+        public object Compile(TextReader sourceCode, ICodeCompilerContext context)
+        {
+            return Compile(compilerService.Factory.CreateStatic(), tempDirectory, sourceCode, context);
+        }
+
+        /// <summary>
+        /// Should try to compile <paramref name="sourceCode"/> into <paramref name="tempDirectory"/> using <paramref name="compiler."/>
+        /// </summary>
+        /// <param name="compiler">C# code compiler.</param>
+        /// <param name="tempDirectory">Directory for storing temp files (.cs, .dll, .pdb, etc).</param>
+        /// <param name="sourceCode">C# source code.</param>
+        /// <param name="context">Code compiler context.</param>
+        /// <returns>Compiled view instance.</returns>
+        protected abstract object Compile(IStaticCompiler compiler, string tempDirectory, TextReader sourceCode, ICodeCompilerContext context);
+    }
+}
