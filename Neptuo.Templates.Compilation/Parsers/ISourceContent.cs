ac925755bb5918f6e5a6f0005013010a0efe7c64
Mara <Neptuo@users.noreply.github.com>
2014-11-04 21:33:00 +0000

    Preparation for replacing string input into IParserService with support for content position info.

---
+++ Neptuo.Templates.Compilation/Parsers/ISourceContent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Templates.Compilation.Parsers
+{
+    /// <summary>
+    /// Describes input into <see cref="IParserService"/>.
+    /// </summary>
+    public interface ISourceContent
+    {
+        /// <summary>
+        /// Gets reader for reading source content.
+        /// </summary>
+        TextReader Content { get; }
+
+        /// <summary>
+        /// Gets information about offset in globally processing content.
+        /// </summary>
+        ISourceLineInfo GlobalSourceInfo { get; }
+    }
+}
---
+++ Neptuo.Templates.Compilation/Parsers/ISourceLineInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Templates.Compilation.Parsers
+{
+    /// <summary>
+    /// Describes position/offset in source global source content.
+    /// </summary>
+    public interface ISourceLineInfo
+    {
+        /// <summary>
+        /// Line number.
+        /// </summary>
+        int LineNumber { get; }
+
+        /// <summary>
+        /// Index at line.
+        /// </summary>
+        int ColumnIndex { get; }
+    }
+}
