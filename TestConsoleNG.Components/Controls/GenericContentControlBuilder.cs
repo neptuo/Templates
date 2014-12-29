08cd5fdf386e05565f728a333195f2d87b16e080
maraf <mara@neptuo.com>
2014-12-29 12:14:00 +0100

    First try for implementing plain text html tags.

--- TestConsoleNG.Components/Controls/GenericContentControlBuilder.cs
+++ TestConsoleNG.Components/Controls/GenericContentControlBuilder.cs
@@ -1,4 +1,6 @@
-﻿using Neptuo.Linq.Expressions;
+﻿using Neptuo;
+using Neptuo.Linq.Expressions;
+using Neptuo.Templates.Compilation;
 using Neptuo.Templates.Compilation.CodeObjects;
 using Neptuo.Templates.Compilation.Parsers;
 using Neptuo.Templates.Controls;
@@ -29,6 +31,49 @@ namespace TestConsoleNG.Controls
             IComponentCodeObject codeObject = base.CreateCodeObject(context, element);
             codeObject.Properties.Add(new SetPropertyDescriptor(new TypePropertyInfo(GetControlType(element).GetProperty(tagNameProperty)), new PlainValueCodeObject(element.Name)));
             return codeObject;
+        }
+
+        public override IEnumerable<ICodeObject> TryParse(IContentBuilderContext context, IXmlElement element)
+        {
+            ComponentCodeObject codeObject = new ComponentCodeObject(typeof(T));
+
+            bool isComponentRequired = false;
+            foreach (IXmlAttribute attribute in element.Attributes)
+            {
+                ICodeObject value = context.TryProcessValue(attribute);
+
+                // Error in parsing.
+                if (value == null)
+                    return null;
+
+                // Is contains extension.
+                ExtensionCodeObject extension = value as ExtensionCodeObject;
+                if (extension != null)
+                {
+                    isComponentRequired = true;
+                    break;
+                }
+
+                if (ObserverFactory.TryParse(context, codeObject, attribute))
+                {
+                    isComponentRequired = true;
+                    break;
+                }
+            }
+
+            if (isComponentRequired)
+                return base.TryParse(context, element);
+            else
+                return ProcessAsTextXmlTag(context, element);
+        }
+
+        protected IEnumerable<ICodeObject> ProcessAsTextXmlTag(IContentBuilderContext context, IXmlElement element)
+        {
+            CodeObjectList result = new CodeObjectList();
+            result.AddPlainValue(String.Format("<{0} {1}>", element.Name, String.Join(" ", element.Attributes)));
+            result.AddRange(context.TryProcessContentNodes(element.ChildNodes));
+            result.AddPlainValue(String.Format("</{0}>", element.Name));
+            return result;
         }
     }
 }
