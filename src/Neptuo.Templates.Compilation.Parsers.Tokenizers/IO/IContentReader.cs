355927326a1d7fd5ff879de33aa5241250a779ff
maraf <mara@neptuo.com>
2015-03-21 11:15:05 +0100

    Base support for tokenizers.

---
+++ src/Neptuo.Templates.Compilation.Parsers.Tokenizers/IO/IContentReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Templates.Compilation.Parsers.Tokenizers.IO
+{
+    /// <summary>
+    /// Describes content reader.
+    /// </summary>
+    public interface IContentReader
+    {
+        /// <summary>
+        /// Current reader position/index.
+        /// </summary>
+        int Position { get; }
+
+        /// <summary>
+        /// Character at index <see cref="IContentReader.Position"/>.
+        /// </summary>
+        char Current { get; }
+
+        /// <summary>
+        /// Tries to read next character.
+        /// Returns <c>true</c> if reading was successfull; <c>false</c> when end of file was reached.
+        /// </summary>
+        /// <returns><c>true</c> if reading was successfull; <c>false</c> when end of file was reached.</returns>
+        bool Next();
+    }
+}
---
+++ src/Neptuo.Templates.Compilation.Parsers.Tokenizers/IO/StringContentReader.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Templates.Compilation.Parsers.Tokenizers.IO
+{
+    /// <summary>
+    /// String content reader.
+    /// Reads characters from string value.
+    /// </summary>
+    public class StringContentReader : IContentReader
+    {
+        /// <summary>
+        /// Default character value when non from input is available.
+        /// </summary>
+        public const char NullChar = '\0';
+
+        private readonly string value;
+
+        public int Position { get; private set; }
+
+        public char Current
+        {
+            get
+            {
+                if (Position < 0)
+                    return NullChar;
+
+                if (value == null)
+                    return NullChar;
+
+                if (Position >= value.Length)
+                    return NullChar;
+
+                return value[Position];
+            }
+        }
+
+        /// <summary>
+        /// Creates new instance which reads characters from <paramref name="value"/>.
+        /// </summary>
+        /// <param name="value">Content characters.</param>
+        public StringContentReader(string value)
+        {
+            this.value = value;
+            Position = -1;
+        }
+
+        public bool Next()
+        {
+            Position++;
+            return Position < value.Length;
+        }
+    }
+}
