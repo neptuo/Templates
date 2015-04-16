0c81be1fa0a9931523177befd018811cbf086867
maraf <mara@neptuo.com>
2015-04-16 20:49:16 +0200

    Remove original curly tokenizer.

--- src/Neptuo.Templates.VisualStudio/IntelliSense/Classifications/TemplateClassifier.cs
+++ src/Neptuo.Templates.VisualStudio/IntelliSense/Classifications/TemplateClassifier.cs
@@ -14,11 +14,11 @@ namespace Neptuo.Templates.VisualStudio.IntelliSense.Classifications
     {
         private readonly ITextBuffer buffer;
         private readonly IClassificationType curlyBrace, curlyContent, curlyError;
-        private readonly CurlyTokenizer tokenizer;
+        //private readonly CurlyTokenizer tokenizer;
 
         public TemplateClassifier(IClassificationTypeRegistryService registry, ITextBuffer buffer)
         {
-            this.tokenizer = new CurlyTokenizer();
+            //this.tokenizer = new CurlyTokenizer();
             this.buffer = buffer;
             curlyBrace = registry.GetClassificationType(TemplateClassificationType.CurlyBrace);
             curlyContent = registry.GetClassificationType(TemplateClassificationType.CurlyContent);
@@ -40,54 +40,54 @@ namespace Neptuo.Templates.VisualStudio.IntelliSense.Classifications
 
             try
             {
-                IList<CurlyToken> tokens = tokenizer.Tokenize(new StringContentReader(buffer.CurrentSnapshot.GetText()), new FakeTokenizerContext());
+                //IList<CurlyToken> tokens = tokenizer.Tokenize(new StringContentReader(buffer.CurrentSnapshot.GetText()), new FakeTokenizerContext());
 
-                CurlyTokenState state = CurlyTokenState.Other;
-                foreach (CurlyToken token in tokens)
-                {
-                    // After error, continue from current state, or start orver?
-                    if (state == CurlyTokenState.Other)
-                    {
-                        if (token.Type == CurlyTokenType.OpenBrace)
-                        {
-                            addSpan(new ClassificationSpan(new SnapshotSpan(span.Snapshot, token.ContentInfo.StartIndex, token.ContentInfo.Length), curlyBrace));
-                            state = CurlyTokenState.Open;
-                        }
-                        else if (token.Type == CurlyTokenType.Error)
-                        {
-                            addSpan(new ClassificationSpan(new SnapshotSpan(span.Snapshot, token.ContentInfo.StartIndex, token.ContentInfo.Length), curlyError));
-                        }
-                    }
-                    else if (state == CurlyTokenState.Open)
-                    {
-                        if (token.Type == CurlyTokenType.Name)
-                        {
-                            addSpan(new ClassificationSpan(new SnapshotSpan(span.Snapshot, token.ContentInfo.StartIndex, token.ContentInfo.Length), curlyContent));
-                            state = CurlyTokenState.Name;
-                        } 
-                        else if (token.Type == CurlyTokenType.CloseBrace)
-                        {
-                            addSpan(new ClassificationSpan(new SnapshotSpan(span.Snapshot, token.ContentInfo.StartIndex, token.ContentInfo.Length), curlyBrace));
-                            state = CurlyTokenState.Other;
-                        }
-                        else if (token.Type == CurlyTokenType.Error)
-                        {
-                            addSpan(new ClassificationSpan(new SnapshotSpan(span.Snapshot, token.ContentInfo.StartIndex, token.ContentInfo.Length), curlyError));
-                        }
-                    }
-                    else if (state == CurlyTokenState.Name)
-                    {
-                        if (token.Type == CurlyTokenType.CloseBrace)
-                        {
-                            addSpan(new ClassificationSpan(new SnapshotSpan(span.Snapshot, token.ContentInfo.StartIndex, token.ContentInfo.Length), curlyBrace));
-                            state = CurlyTokenState.Other;
-                        }
-                        else if(token.Type == CurlyTokenType.Error)
-                        {
-                            addSpan(new ClassificationSpan(new SnapshotSpan(span.Snapshot, token.ContentInfo.StartIndex, token.ContentInfo.Length), curlyError));
-                        }
-                    }
-                }
+                //CurlyTokenState state = CurlyTokenState.Other;
+                //foreach (CurlyToken token in tokens)
+                //{
+                //    // After error, continue from current state, or start orver?
+                //    if (state == CurlyTokenState.Other)
+                //    {
+                //        if (token.Type == CurlyTokenType.OpenBrace)
+                //        {
+                //            addSpan(new ClassificationSpan(new SnapshotSpan(span.Snapshot, token.ContentInfo.StartIndex, token.ContentInfo.Length), curlyBrace));
+                //            state = CurlyTokenState.Open;
+                //        }
+                //        else if (token.Type == CurlyTokenType.Error)
+                //        {
+                //            addSpan(new ClassificationSpan(new SnapshotSpan(span.Snapshot, token.ContentInfo.StartIndex, token.ContentInfo.Length), curlyError));
+                //        }
+                //    }
+                //    else if (state == CurlyTokenState.Open)
+                //    {
+                //        if (token.Type == CurlyTokenType.Name)
+                //        {
+                //            addSpan(new ClassificationSpan(new SnapshotSpan(span.Snapshot, token.ContentInfo.StartIndex, token.ContentInfo.Length), curlyContent));
+                //            state = CurlyTokenState.Name;
+                //        } 
+                //        else if (token.Type == CurlyTokenType.CloseBrace)
+                //        {
+                //            addSpan(new ClassificationSpan(new SnapshotSpan(span.Snapshot, token.ContentInfo.StartIndex, token.ContentInfo.Length), curlyBrace));
+                //            state = CurlyTokenState.Other;
+                //        }
+                //        else if (token.Type == CurlyTokenType.Error)
+                //        {
+                //            addSpan(new ClassificationSpan(new SnapshotSpan(span.Snapshot, token.ContentInfo.StartIndex, token.ContentInfo.Length), curlyError));
+                //        }
+                //    }
+                //    else if (state == CurlyTokenState.Name)
+                //    {
+                //        if (token.Type == CurlyTokenType.CloseBrace)
+                //        {
+                //            addSpan(new ClassificationSpan(new SnapshotSpan(span.Snapshot, token.ContentInfo.StartIndex, token.ContentInfo.Length), curlyBrace));
+                //            state = CurlyTokenState.Other;
+                //        }
+                //        else if(token.Type == CurlyTokenType.Error)
+                //        {
+                //            addSpan(new ClassificationSpan(new SnapshotSpan(span.Snapshot, token.ContentInfo.StartIndex, token.ContentInfo.Length), curlyError));
+                //        }
+                //    }
+                //}
             }
             catch (Exception ex)
             {
