3a54e15ec7e12c2051613b9e2f8be12db6d58515
maraf <mara@neptuo.com>
2015-04-27 15:10:44 +0200

    Tokenization of <empty /> base empty tag.

--- src/Neptuo.Templates.Compilation.Parsers.Tokenizers.Composable/AngleTokenType.cs
+++ src/Neptuo.Templates.Compilation.Parsers.Tokenizers.Composable/AngleTokenType.cs
@@ -9,7 +9,16 @@ namespace Neptuo.Templates.Compilation.Parsers.Tokenizers
     public class AngleTokenType : ComposableTokenType
     {
         public static readonly ComposableTokenType OpenBrace = new ComposableTokenType("Angle.OpenBrace");
+        public static readonly ComposableTokenType NamePrefix = new ComposableTokenType("Angle.NamePrefix");
+        public static readonly ComposableTokenType NameSeparator = new ComposableTokenType("Angle.NameSeparator");
+        public static readonly ComposableTokenType Name = new ComposableTokenType("Angle.Name");
+        public static readonly ComposableTokenType SelfCloseBrace = new ComposableTokenType("Angle.SelfCloseBrace");
         public static readonly ComposableTokenType CloseBrace = new ComposableTokenType("Angle.CloseBrace");
+
+        public static readonly ComposableTokenType OpenDirective = new ComposableTokenType("Angle.OpenDirective");
+
+        public static readonly ComposableTokenType OpenComment = new ComposableTokenType("Angle.OpenComment");
+        public static readonly ComposableTokenType CloseComment = new ComposableTokenType("Angle.CloseComment");
 
         private AngleTokenType() 
             : base("")
--- src/Neptuo.Templates.Compilation.Parsers.Tokenizers.Composable/AngleTokenizer.cs
+++ src/Neptuo.Templates.Compilation.Parsers.Tokenizers.Composable/AngleTokenizer.cs
@@ -25,17 +25,58 @@ namespace Neptuo.Templates.Compilation.Parsers.Tokenizers
 
         private bool ChooseNodeType(ContentDecorator decorator, IComposableTokenizerContext context, List<ComposableToken> result)
         {
+            // <
             if (!decorator.Next())
                 return false;
 
+            // <[a-Z]
             if (Char.IsLetter(decorator.Current))
             {
-                CreateToken(decorator, result, AngleTokenType.OpenBrace)
+                CreateToken(decorator, result, AngleTokenType.OpenBrace);
                 return ReadElementName(decorator, context, result);
             }
+            // <!
             else if (decorator.Current == '!')
             {
-                return ReadDirectiveName(decorator, context, result);
+                if (decorator.Next())
+                {
+                    // <!-
+                    if (decorator.Current == '-')
+                    {
+                        // <!--
+                        if (decorator.Next() && decorator.Current == '-')
+                        {
+                            CreateToken(decorator, result, AngleTokenType.OpenComment);
+                            ReadComment(decorator, context, result);
+                        }
+                        else
+                        {
+                            CreateToken(decorator, result, AngleTokenType.Error);
+                            return false;
+                        }
+                    }
+                    // <![a-Z
+                    else if(Char.IsLetter(decorator.Current))
+                    {
+                        CreateToken(decorator, result, AngleTokenType.OpenDirective);
+                        return ReadDirectiveName(decorator, context, result);
+                    }
+                    else
+                    {
+                        CreateToken(decorator, result, AngleTokenType.Error);
+                        return false;
+                    }
+                }
+                else
+                {
+                    CreateToken(decorator, result, AngleTokenType.Error);
+                    return false;
+                }
+            }
+            else
+            {
+                CreateToken(decorator, result, AngleTokenType.Error);
+                return false;
             }
 
             throw Ensure.Exception.NotImplemented();
@@ -43,10 +84,57 @@ namespace Neptuo.Templates.Compilation.Parsers.Tokenizers
 
         private bool ReadElementName(ContentDecorator decorator, IComposableTokenizerContext context, List<ComposableToken> result)
         {
-            throw Ensure.Exception.NotImplemented();
+            if (decorator.CurrentWhile(Char.IsLetterOrDigit))
+            {
+                if (decorator.Current == ':')
+                {
+                    CreateToken(decorator, result, AngleTokenType.NamePrefix, 1);
+                    CreateToken(decorator, result, AngleTokenType.NameSeparator);
+                    if (decorator.NextWhile(Char.IsLetterOrDigit))
+                    {
+                        CreateToken(decorator, result, AngleTokenType.Name);
+                    } 
+                    else 
+                    {
+                        CreateToken(decorator, result, AngleTokenType.Error);
+                        return false;
+                    }
+                }
+
+                if (decorator.CurrentWhile(Char.IsWhiteSpace))
+                    CreateToken(decorator, result, AngleTokenType.Whitespace);
+
+                if (decorator.Current == '/')
+                {
+                    if (decorator.Next())
+                    {
+                        if (decorator.Current == '>')
+                        {
+                            CreateToken(decorator, result, AngleTokenType.SelfCloseBrace);
+                        }
+                        else
+                        {
+                            CreateToken(decorator, result, AngleTokenType.Error);
+                            return false;
+                        }
+                    }
+                }
+                else if (decorator.Current == '>')
+                {
+                    CreateToken(decorator, result, AngleTokenType.CloseBrace);
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         private bool ReadDirectiveName(ContentDecorator decorator, IComposableTokenizerContext context, List<ComposableToken> result)
+        {
+            throw Ensure.Exception.NotImplemented();
+        }
+
+        private bool ReadComment(ContentDecorator decorator, IComposableTokenizerContext context, List<ComposableToken> result)
         {
             throw Ensure.Exception.NotImplemented();
         }
