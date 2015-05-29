c5333b6ff1735c22ff72ea8865e9445cf64bbe36
maraf <mara@neptuo.com>
2015-05-29 10:42:14 +0200

    Project for performance testing.

---
+++ test/PerfTest.Templates.Parsers.Tokenizers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfTest.Templates.Parsers.Tokenizers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
