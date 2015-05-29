efc190aacd4faf8f5f026ed6dbd7aa1281c6d578
maraf <mara@neptuo.com>
2015-05-29 10:53:12 +0200

    Base perf test implementation.

--- test/PerfTest.Templates.Parsers.Tokenizers/Program.cs
+++ test/PerfTest.Templates.Parsers.Tokenizers/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Neptuo.Diagnostics;
+using Neptuo.Templates.Compilation.Parsers.Tokenizers;
+using Neptuo.Templates.Compilation.Parsers.Tokenizers.IO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +9,37 @@ using System.Threading.Tasks;
 
 namespace PerfTest.Templates.Parsers.Tokenizers
 {
-    class Program
+    class Program : DebugBase
     {
         static void Main(string[] args)
         {
+            new Program()
+                .BaseTest();
 
+            Console.ReadKey(true);
+        }
+
+        public Program()
+            : base(Console.Out)
+        { }
+
+        ComposableTokenizer CreateTokenizer()
+        {
+            ComposableTokenizer tokenizer = new ComposableTokenizer();
+            tokenizer.Add(new CurlyTokenizer());
+            tokenizer.Add(new PlainTokenizer());
+            return tokenizer;
+        }
+
+        void BaseTest()
+        {
+            string content = "Hello, {Binding ID, Converter=Static} asd jalksdj alksjdlka {Binding ID, Converter=Static} askdj lasdjalk djaskldj alskdj asldkja {Binding Converter=Static} {Binding Converter=Dynamic}...";
+            for (int i = 0; i < 8; i++)
+                content += content;
+
+            Console.WriteLine(content.Length);
+            ComposableTokenizer tokenizer = CreateTokenizer();
+            IList<ComposableToken> tokens = Debug("Composable", () => tokenizer.Tokenize(new StringReader(content), new FakeTokenizerContext()));
         }
     }
 }
---
+++ test/PerfTest.Templates.Parsers.Tokenizers/_Fakes/FakeTokenizerContext.cs
@@ -0,0 +1,53 @@
+﻿using Neptuo.Activators;
+using Neptuo.ComponentModel;
+using Neptuo.Templates.Compilation.Parsers.Tokenizers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfTest.Templates.Parsers.Tokenizers
+{
+    internal class FakeTokenizerContext : ITokenizerContext
+    {
+        public IDependencyProvider DependencyProvider
+        {
+            get { return new FakeDependencyProvider(); }
+        }
+
+        public ICollection<IErrorInfo> Errors
+        {
+            get { return new List<IErrorInfo>(); }
+        }
+    }
+
+    class FakeDependencyProvider : IDependencyProvider
+    {
+        public object Resolve(Type requiredType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDependencyContainer Scope(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ScopeName
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsDisposed
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
