4c10693708827bd15291b7266549c99c9beecf86
maraf <mara@neptuo.com>
2015-06-12 23:12:44 +0200

    Basic support for literal syntax.

---
+++ src/Neptuo.Templates.Compilation.Parsers/_Syntax/TextSyntax.cs
@@ -0,0 +1,32 @@
+﻿using Neptuo.Templates.Compilation.Parsers.Tokenizers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Templates.Compilation.Parsers
+{
+    public class TextSyntax : ISyntaxNode
+    {
+        public IReadOnlyList<ComposableToken> Tokens { get; private set; }
+
+        public ComposableToken TextToken { get; private set; }
+
+        public TextSyntax(ComposableToken textToken)
+        {
+            TextToken = textToken;
+        }
+
+        public TextSyntax WithText(ComposableToken textToken)
+        {
+            return new TextSyntax(textToken);
+        }
+
+
+        public static TextSyntax New()
+        {
+            return new TextSyntax(null);
+        }
+    }
+}
---
+++ src/Neptuo.Templates.Compilation.Parsers/_Syntax/TextSyntaxBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Neptuo.Templates.Compilation.Parsers.Tokenizers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Templates.Compilation.Parsers
+{
+    public class TextSyntaxBuilder : ISyntaxBuilder
+    {
+        public ISyntaxNode Build(IList<ComposableToken> tokens, int startIndex)
+        {
+            ComposableToken token = tokens[startIndex];
+            if (token.Type == ComposableTokenType.Text)
+                return TextSyntax.New().WithText(token);
+
+            throw new NotImplementedException();
+        }
+    }
+}
