7cb79d698b6bec3375bdb444d26241b3978ba1cd
maraf <mara@neptuo.com>
2015-06-12 23:09:03 +0200

    Basic curly syntax tree.

---
+++ src/Neptuo.Templates.Compilation.Parsers/_Syntax/CurlySyntax.cs
@@ -0,0 +1,108 @@
+﻿using Neptuo.Templates.Compilation.Parsers.Tokenizers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Templates.Compilation.Parsers
+{
+    public class CurlySyntax : ISyntaxNode
+    {
+        public IReadOnlyList<ComposableToken> Tokens { get; private set; }
+
+        public ComposableToken OpenToken { get; private set; }
+        public CurlyNameSyntax Name { get; private set; }
+        public ComposableToken CloseToken { get; private set; }
+
+        internal CurlySyntax(ComposableToken openToken, CurlyNameSyntax name, ComposableToken closeToken)
+        {
+            OpenToken = openToken;
+            Name = name;
+            CloseToken = closeToken;
+            List<ComposableToken> tokens = new List<ComposableToken>();
+
+            if (openToken != null)
+                tokens.Add(openToken);
+
+            if (name != null)
+                tokens.AddRange(name.Tokens);
+
+            if (closeToken != null)
+                tokens.Add(closeToken);
+
+            Tokens = tokens;
+        }
+
+        public CurlySyntax WithOpenToken(ComposableToken openToken)
+        {
+            return new CurlySyntax(openToken, Name, CloseToken);
+        }
+
+        public CurlySyntax WithName(CurlyNameSyntax name)
+        {
+            return new CurlySyntax(OpenToken, name, CloseToken);
+        }
+
+        public CurlySyntax WithCloseToken(ComposableToken closeToken)
+        {
+            return new CurlySyntax(OpenToken, Name, closeToken);
+        }
+
+
+
+        public static CurlySyntax New()
+        {
+            return new CurlySyntax(null, null, null);
+        }
+    }
+
+    public class CurlyNameSyntax : ISyntaxNode
+    {
+        public IReadOnlyList<ComposableToken> Tokens { get; private set; }
+        
+        public ComposableToken Prefix { get; private set; }
+        public ComposableToken NameSeparator { get; private set; }
+        public ComposableToken Name { get; private set; }
+
+        internal CurlyNameSyntax(ComposableToken prefix, ComposableToken nameSeparator, ComposableToken name)
+        {
+            Prefix = prefix;
+            NameSeparator = nameSeparator;
+            Name = name;
+
+            List<ComposableToken> tokens = new List<ComposableToken>();
+            if (prefix != null)
+                tokens.Add(prefix);
+
+            if (nameSeparator != null)
+                tokens.Add(nameSeparator);
+
+            if (name != null)
+                tokens.Add(name);
+
+            Tokens = tokens;
+        }
+
+        public CurlyNameSyntax WithPrefix(ComposableToken prefix)
+        {
+            return new CurlyNameSyntax(prefix, NameSeparator, Name);
+        }
+
+        public CurlyNameSyntax WithNameSeparator(ComposableToken nameSeparator)
+        {
+            return new CurlyNameSyntax(Prefix, nameSeparator, Name);
+        }
+
+        public CurlyNameSyntax WithName(ComposableToken name)
+        {
+            return new CurlyNameSyntax(Prefix, NameSeparator, name);
+        }
+
+
+        public static CurlyNameSyntax New()
+        {
+            return new CurlyNameSyntax(null, null, null);
+        }
+    }
+}
---
+++ src/Neptuo.Templates.Compilation.Parsers/_Syntax/CurlySyntaxBuilder.cs
@@ -0,0 +1,90 @@
+﻿using Neptuo.Templates.Compilation.Parsers.Tokenizers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Templates.Compilation.Parsers
+{
+    public class CurlySyntaxBuilder : ISyntaxBuilder
+    {
+        public ISyntaxNode Build(IList<ComposableToken> tokens, int startIndex)
+        {
+            CurlySyntax result = CurlySyntax.New();
+
+            IEnumerator<ComposableToken> enumerator = new ComposableTokenEnumerator(tokens, startIndex);
+            ComposableToken token = enumerator.Current;
+            if (token.Type == CurlyTokenType.OpenBrace)
+            {
+                result = result.WithOpenToken(token);
+                return BuildName(enumerator, result);
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+
+            return result;
+        }
+
+        private ISyntaxNode BuildName(IEnumerator<ComposableToken> enumerator, CurlySyntax result)
+        {
+            if (enumerator.MoveNext())
+            {
+                CurlyNameSyntax name = CurlyNameSyntax.New();
+                if (enumerator.Current.Type == CurlyTokenType.NamePrefix)
+                {
+                    name = name.WithPrefix(enumerator.Current);
+                    if (enumerator.MoveNext() && enumerator.Current.Type == CurlyTokenType.NameSeparator)
+                    {
+                        name = name.WithNameSeparator(enumerator.Current);
+                        if (enumerator.MoveNext() && enumerator.Current.Type == CurlyTokenType.Name)
+                            name = name.WithName(enumerator.Current);
+                        else
+                            throw new NotImplementedException();
+                    }
+                    else
+                    {
+                        throw new NotImplementedException();
+                    }
+                }
+                else if (enumerator.Current.Type == CurlyTokenType.Name)
+                {
+                    name = name.WithName(enumerator.Current);
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
+
+                result.WithName(name);
+                return BuildContent(enumerator, result);
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private ISyntaxNode BuildContent(IEnumerator<ComposableToken> enumerator, CurlySyntax result)
+        {
+            if (enumerator.MoveNext())
+            {
+                if(enumerator.Current.Type == CurlyTokenType.CloseBrace)
+                {
+                    result.WithCloseToken(enumerator.Current);
+                    return result;
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
