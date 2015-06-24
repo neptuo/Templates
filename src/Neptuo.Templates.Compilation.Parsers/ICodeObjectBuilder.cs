76e7bf2efad655f681c2dec9ed24f33b81a86a4e
maraf <mara@neptuo.com>
2015-06-24 14:45:53 +0200

    Defining interface for code object builder.

---
+++ src/Neptuo.Templates.Compilation.Parsers/ICodeObjectBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Neptuo.Templates.Compilation.CodeObjects;
+using Neptuo.Templates.Compilation.Parsers.SyntaxTrees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Templates.Compilation.Parsers
+{
+    public interface ICodeObjectBuilder
+    {
+        IEnumerable<ICodeObject> TryBuild(ISyntaxNode node, ICodeObjectBuilderContext context);
+    }
+}
