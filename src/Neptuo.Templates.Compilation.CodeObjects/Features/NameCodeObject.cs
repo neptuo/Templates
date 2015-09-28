e327845c7c2b36f4b61c73611d51d0397417287b
maraf <mara@neptuo.com>
2015-09-28 11:19:36 +0200

    Add named code object.

---
+++ src/Neptuo.Templates.Compilation.CodeObjects/Features/NameCodeObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Templates.Compilation.CodeObjects.Features
+{
+    /// <summary>
+    /// Default implementation of <see cref="INameCodeObject"/>.
+    /// </summary>
+    public class NameCodeObject : INameCodeObject
+    {
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        /// <param name="name">Name of the code object.</param>
+        public NameCodeObject(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ src/Neptuo.Templates.Compilation.CodeObjects/INameCodeObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Templates.Compilation.CodeObjects
+{
+    /// <summary>
+    /// Defines code object which takes unique name.
+    /// </summary>
+    public interface INameCodeObject : ICodeObject
+    {
+        /// <summary>
+        /// Name of the code object.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
