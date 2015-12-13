1dc0db6174c8d1b470569058cd7ee2035f5a0943
maraf <mara@neptuo.com>
2015-12-13 10:09:33 +0100

    Add angle syntax nodes.

---
+++ src/Neptuo.Templates.Compilation.Parsers.Syntax/Nodes/AngleSyntaxNodeBuilder.cs
@@ -0,0 +1,39 @@
+﻿using Neptuo.Templates.Compilation.Parsers.Syntax.Tokenizers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Templates.Compilation.Parsers.Syntax.Nodes
+{
+    public class AngleSyntaxNodeBuilder : ISyntaxNodeBuilder
+    {
+        public ISyntaxNode Build(IList<Token> tokens, int startIndex, ISyntaxNodeBuilderContext context)
+        {
+            CurlySyntax result = new CurlySyntax();
+
+            TokenListReader reader = new TokenListReader(tokens, startIndex);
+            Token token = reader.Current;
+
+            while (token.Type == CurlyTokenType.Whitespace)
+            {
+                result.LeadingTrivia.Add(token);
+                if (!reader.Next())
+                    throw new NotImplementedException();
+
+                token = reader.Current;
+            }
+
+            if (token.Type == CurlyTokenType.OpenBrace)
+            {
+                result.OpenToken = token;
+                return BuildName(reader, result, context);
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+        }
+    }
+}
---
+++ src/Neptuo.Templates.Compilation.Parsers.Syntax/Nodes/AngleSyntaxNodes.cs
@@ -0,0 +1,139 @@
+﻿using Neptuo.Templates.Compilation.Parsers.Syntax.Tokenizers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.Templates.Compilation.Parsers.Syntax.Nodes
+{
+    public class AngleSyntax : SyntaxNodeBase<AngleSyntax>
+    {
+        public Token OpenToken { get; set; }
+        public AngleNameSyntax Name { get; set; }
+        public Token SelfCloseToken { get; set; }
+        public Token CloseToken { get; set; }
+        public IList<CurlyAttributeSyntax> Attributes { get; set; }
+
+        protected override AngleSyntax CloneInternal()
+        {
+            AngleSyntax result = new AngleSyntax
+            {
+                OpenToken = OpenToken,
+                Name = Name != null ? Name.Clone() : null,
+                SelfCloseToken = SelfCloseToken,
+                CloseToken = CloseToken
+            };
+
+            if (Attributes.Count > 0)
+                result.Attributes.AddRange(Attributes.Select(a => a.Clone()));
+
+            return result;
+        }
+
+        protected override IEnumerable<Token> GetTokensInternal()
+        {
+            List<Token> result = new List<Token>();
+
+            if (OpenToken != null)
+                result.Add(OpenToken);
+
+            if (Name != null)
+                result.AddRange(Name.GetTokens());
+
+            if (Attributes.Count > 0)
+                result.AddRange(Attributes.SelectMany(a => a.GetTokens()));
+
+            if (CloseToken != null)
+                result.Add(CloseToken);
+
+            if (SelfCloseToken != null)
+                result.Add(SelfCloseToken);
+
+            return result;
+        }
+    }
+
+    public class AngleNameSyntax : SyntaxNodeBase<AngleNameSyntax>
+    {
+        public Token PrefixToken { get; set; }
+        public Token NameSeparatorToken { get; set; }
+        public Token NameToken { get; set; }
+
+        protected override AngleNameSyntax CloneInternal()
+        {
+            return new AngleNameSyntax
+            {
+                PrefixToken = PrefixToken,
+                NameSeparatorToken = NameSeparatorToken,
+                NameToken = NameToken
+            };
+        }
+
+        protected override IEnumerable<Token> GetTokensInternal()
+        {
+            List<Token> result = new List<Token>();
+
+            if (PrefixToken != null)
+                result.Add(PrefixToken);
+
+            if (NameSeparatorToken != null)
+                result.Add(NameSeparatorToken);
+
+            if (NameToken != null)
+                result.Add(NameToken);
+
+            return result;
+        }
+    }
+
+    public class AngleAttributeSyntax : SyntaxNodeBase<AngleAttributeSyntax>
+    {
+        public AngleNameSyntax Name { get; set; }
+        public Token ValueSeparatorToken { get; set; }
+        public Token AttributeOpenValueToken { get; set; }
+        public ISyntaxNode Value { get; set; }
+        public Token AttributeCloseValueToken { get; set; }
+
+        protected override AngleAttributeSyntax CloneInternal()
+        {
+            AngleAttributeSyntax result = new AngleAttributeSyntax
+            {
+                Name = Name.Clone(),
+                ValueSeparatorToken = ValueSeparatorToken,
+                AttributeOpenValueToken = AttributeOpenValueToken,
+                AttributeCloseValueToken = AttributeCloseValueToken
+            };
+
+            ICloneable<ISyntaxNode> cloneableValue = Value as ICloneable<ISyntaxNode>;
+            if (cloneableValue != null)
+                result.Value = cloneableValue.Clone();
+            else if (Value != null)
+                throw Ensure.Condition.NotCloneable(Value);
+
+            return result;
+        }
+
+        protected override IEnumerable<Token> GetTokensInternal()
+        {
+            List<Token> result = new List<Token>();
+
+            if (Name != null)
+                result.AddRange(Name.GetTokens());
+
+            if (ValueSeparatorToken != null)
+                result.Add(ValueSeparatorToken);
+
+            if (AttributeOpenValueToken != null)
+                result.Add(AttributeOpenValueToken);
+
+            if (Value != null)
+                result.AddRange(Value.GetTokens());
+
+            if (AttributeCloseValueToken != null)
+                result.Add(AttributeCloseValueToken);
+
+            return result;
+        }
+    }
+}
